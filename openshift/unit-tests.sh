d990061d8da4f8301e165c533375168bcaa1b0e5
Damiano Donati <damiano.donati@gmail.com>
2025-10-10 14:58:47 +0200

    UPSTREAM: <carry>: add mockgen/setup-envtest installation to openshift/unit-tests.sh

--- openshift/unit-tests.sh
+++ openshift/unit-tests.sh
@@ -11,4 +11,14 @@ if [ $HOME == "/" ]; then
   export HOME=/tmp/kubebuilder/testing
 fi
 
+# mockgen must support the generic prism-go-client interfaces used in this branch.
+MOCKGEN_VERSION="v0.6.0"
+echo "Installing mockgen@${MOCKGEN_VERSION}"
+go install -mod=mod "go.uber.org/mock/mockgen@${MOCKGEN_VERSION}"
+
+# setup-envtest is implicitly available in the nutanix-cloud-native/cluster-api-provider-nutanix CI environment.
+SETUP_ENVTEST_VERSION="release-0.16"
+echo "Installing setup-envtest@${SETUP_ENVTEST_VERSION}"
+go install -mod=mod "sigs.k8s.io/controller-runtime/tools/setup-envtest@${SETUP_ENVTEST_VERSION}"
+
 GOFLAGS='-mod=readonly' make unit-test
